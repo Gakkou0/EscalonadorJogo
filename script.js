e7bddcb94b06f5b431cbefa61ec2929474482d1d
Gakkou0 <fhugo.625@gmail.com>
2023-04-13 17:52:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const stress = document.querySelector(".stressBarComplete")
+const input = 0
+const maxValue = document.querySelector(".maxValue")
+let finalValue = 0
+let max = 0
+
+maxValue.addEventListener("keyup", function() {
+    max = parseInt(maxValue.value, 10)
+    console.log("max", max)
+})
+
