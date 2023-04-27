aa99818b42f79f17fce10a839d02030a09d97f2d
Gakkou0 <fhugo.625@gmail.com>
2023-04-26 22:37:07 -0300

    organização

---
+++ server.js
@@ -0,0 +1,7 @@
+var express = require('express');
+// Initialise Express
+var app = express();
+// Render static files
+app.use(express.static('public'));
+// Port website will run on
+app.listen(8080);
