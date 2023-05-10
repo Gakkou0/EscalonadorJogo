f9cb4de66f3b16db5cb4687e53e751a5633d45fc
Gakkou0 <fhugo.625@gmail.com>
2023-05-10 16:48:24 -0300

    refatoração

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
-// Initialise Express
 const app = express();
-// Render static files
-app.use(express.static('public'));
-// Port website will run on
+
+//app.use(express.static('public'));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+app.get("/cadastro", function(req, res){
+    res.sendFile(__dirname + "/public/singup.html")
+})
+
+app.get("/login", function(req, res){
+    res.sendFile(__dirname + "/public/login.html")
+})
+
 app.listen(8080);
