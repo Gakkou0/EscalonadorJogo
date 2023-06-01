d20819b317c25bc882f8631a528fe609a50994b6
Gakkou0 <fhugo.625@gmail.com>
2023-06-01 15:18:54 -0300

    refatoração

--- scripts/db.js
+++ scripts/db.js
@@ -1,51 +1,40 @@
-const ThenPromise = require('promise');
+const mysql = require('mysql2/promise');
 
 const connecting = async () => {
-  if(global.connection && global.connection.state !== "disconnected"){
-    return global.connection
+  if (global.connection && global.connection.state !== "disconnected") {
+    return global.connection;
   }
 
-  const mysql = require('mysql2/promise')
-  const connection = mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'escalonador'
-  })
-
-  connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
-   
-    console.log('connected as id ' + connection.threadId);
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'escalonador'
   });
 
-  global.connection = connection
-  return connection
+  console.log('connected as id ' + connection.threadId);
+  global.connection = connection;
+  return connection;
+};
+
+async function insertPlayer(player) {
+  const conn = await connecting();
+  const sql = 'INSERT INTO player (name, userName, email, password, matchesCampaingMode, matchesSurvivalMode, survivalHighScore, campaingHighScore, campaingDefeat) VALUES (?,?,?,?,?,?,?,?,?)';
+  const values = [player.name, player.userName, player.email, player.password, player.matchesCampaingMode, player.matchesSurvivalMode, player.survivalHighScore, player.campaingHighScore, player.campaingDefeat];
+  await conn.query(sql, values);
 }
 
-
-async function insertPlayer(player){
-  const conn = await connecting()
-  const sql = 'INSERT INTO player(name, userName, email, password, matchesCampaingMode, matchesSurvivalMode, survivalHighScore, campaingHighScore, campaingDefeat) VALUES (?,?,?,?,?,?,?,?,?)'
-  const values = [player.name, player.userName, player.email, player.password, player.matchesCampaingMode, player.matchesSurvivalMode, player.survivalHighScore, player.campaingHighScore, player.campaingDefeat]
-  conn.query(sql,values)
+async function deletePlayer(value) {
+  const conn = await connecting();
+  const sql = 'DELETE FROM player WHERE playerId=?';
+  await conn.query(sql, [value]);
 }
 
-async function deletePlayer(value){
-  const conn = await connecting()
-  const sql = 'DELETE FROM player WHERE playerId=?;'
-  conn.query(sql,[value])
+async function updatePlayer(value, player) {
+  const conn = await connecting();
+  const sql = 'UPDATE player SET name=?, userName=?, email=?, password=?, matchesCampaingMode=?, matchesSurvivalMode=?, survivalHighScore=?, campaingHighScore=?, campaingDefeat=? WHERE playerId=?';
+  const values = [player.name, player.userName, player.email, player.password, player.matchesCampaingMode, player.matchesSurvivalMode, player.survivalHighScore, player.campaingHighScore, player.campaingDefeat, value];
+  await conn.query(sql, values);
 }
 
-async function updatePlayer(value, player){
-    const conn = await connect();
-    const sql = 'UPDATE player SET name = ?, userName = ?, email = ?, password = ?, matchesCampaingMode = ?, matchesSurvivalMode = ?, survivalHighScore = ?, campaingHighScore = ?, campaingDefeat = ? WHERE playerId=?';
-    const values = [player.name, player.userName, player.email, player.password, player.matchesCampaingMode, player.matchesSurvivalMode, player.survivalHighScore, player.campaingHighScore, player.campaingDefeat,value];
-    return await conn.query(sql, values);
-}
-
-module.exports = {insertPlayer, deletePlayer, updatePlayer}
-
+module.exports = { insertPlayer, deletePlayer, updatePlayer };
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-(async ()=>{
-    const db = require('./scripts/db')
-})()
-
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-//app.use(express.static('public'));
+const db = require('./scripts/db');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.use(express.static('public'));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/public/index.html")
@@ -13,10 +15,30 @@ app.get("/", function(req, res){
 
 app.get("/cadastro", function(req, res){
     res.sendFile(__dirname + "/public/singup.html")
+
+})
+
+app.post("/cadastro", function(req, res){
+    const playerData = req.body;
+    console.log(req)
+
+    db.insertPlayer(playerData)
+      .then(() => {
+        res.send('Jogador adicionado com sucesso');
+      })
+      .catch((error) => {
+        console.log(error)
+        res.status(500).send('Erro ao adicionar jogador');
+      });
 })
 
 app.get("/login", function(req, res){
     res.sendFile(__dirname + "/public/login.html")
 })
+
+app.get("/game", function(req, res){
+  res.sendFile(__dirname + "/public/game.html")
+})
+
 
 app.listen(8080);
